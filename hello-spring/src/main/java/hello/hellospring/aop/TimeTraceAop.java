22235d48f42981f4510afe7334b3e42a2cbeb725
seinpyo <91402281+seinpyo@users.noreply.github.com>
2023-05-01 21:35:42 +0900

    test commit

--- hello-spring/src/main/java/hello/hellospring/aop/TimeTraceAop.java
+++ hello-spring/src/main/java/hello/hellospring/aop/TimeTraceAop.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 @Aspect
 @Component
 public class TimeTraceAop {
-    @Around("execution(* hello.hellospring..*(..))")
+    @Around("execution(* hello.hellospring.service..*(..))")
     public Object excute(ProceedingJoinPoint joinPoint) throws Throwable {
         long start = System.currentTimeMillis();
         System.out.println("START: " + joinPoint.toString()); //메서드 이름
