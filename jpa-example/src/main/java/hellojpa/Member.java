2aab6388fce311fb73b1392bb93db80823ea46ed
seinpyo <91402281+seinpyo@users.noreply.github.com>
2023-05-24 22:35:04 +0900

    @Entity

--- jpa-example/src/main/java/hellojpa/Member.java
+++ jpa-example/src/main/java/hellojpa/Member.java
@@ -6,11 +6,18 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
+//JPA를 사용해서 테이블과 매핑할 클래스는 @Entity 필수
+//기본생성자 필수
 public class Member {
     @Id
     private Long id;
-
     private String name;
+
+    public Member() {}
+    public Member(Long id, String name) {
+        this.id = id;
+        this.name = name ;
+    }
 
     public Long getId() {
         return id;
